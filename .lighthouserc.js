5e4f7a3daf2f499f61da39319327b8f4f45058ae
raon.c <raon.c@kakaoent.com>
2022-04-29 14:34:07 +0900

    first commit

---
+++ .lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ["http://localhost:3000"],
+      collect: {
+        numberOfRuns: 5,
+      },
+    },
+    upload: {
+      startServerCommnad: "npm run start",
+      target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
+    },
+  },
+};
