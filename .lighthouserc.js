a0008cdf83e989522348645dc9e48cda60f5ef5d
raon.c <raon.c@kakaoent.com>
2022-04-29 17:02:57 +0900

    lighthouserc 파일 수정

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,10 +1,14 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: "./.next",
+      startServerCommand: "npm run start", // 서버를 키는 명령어
+      url: ["http://localhost:3000"],
+      collect: {
+        numberOfRuns: 5, // Lighthouse 가 5번 실행됨
+      },
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "temporary-public-storage", // Lighthouse 서버에 결과가 업로드됨 명령어 실행 후 확인할 수 있는 link가 생성됨
     },
   },
 };
