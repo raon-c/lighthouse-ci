8be1b95d88c8a729e43b5526cbea4018a063b0d4
raon.c <raon.c@kakaoent.com>
2022-05-02 16:32:45 +0900

    comment 테스트

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,11 +4,14 @@ module.exports = {
       startServerCommand: "npm run start", // 서버를 키는 명령어
       url: ["http://localhost:3000", "http://localhost:3000/ci"],
       collect: {
-        numberOfRuns: 5, // Lighthouse 가 5번 실행됨
+        numberOfRuns: 1, // Lighthouse 가 5번 실행됨
       },
     },
     upload: {
-      target: "temporary-public-storage", // Lighthouse 서버에 결과가 업로드됨 명령어 실행 후 확인할 수 있는 link가 생성됨
+      //   target: "temporary-public-storage", // Lighthouse 서버에 결과가 업로드됨 명령어 실행 후 확인할 수 있는 link가 생성됨
+      target: "filesystem",
+      outputDir: "./lhci_reports",
+      reportFilenamePattern: "%%PATHNAME%%-report.%%EXTENSION%%",
     },
   },
 };
